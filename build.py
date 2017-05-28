76118e825021f253c073f9ba10c89cb976bc6335
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-05-28 21:50:28 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,17 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    dic.items().sort
+    return dic.items()
 
 
 def solution_desc(dic):
     '''
     Enter your code here
     '''
+    descList = dic.items()
+    descList.reverse()
+    return  descList
 
+solution_asc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
+solution_desc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
