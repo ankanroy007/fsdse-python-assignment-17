fb7f7e86af41885f28f7b5e761c18a4d1d82c19d
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-05-28 22:12:49 +0000

    Done

--- build.py
+++ build.py
@@ -5,17 +5,16 @@ def solution_asc(dic):
     '''dic.items().sort
     return dic.items()'''
 
-    ascDic = sorted(dic.items(),key = operator.itemgetter(2))
-    print ascDic
+    ascDic = sorted(dic.items(),key = operator.itemgetter(0))
+    return ascDic
 
 
 def solution_desc(dic):
     '''
     Enter your code here
     '''
-    descList = dic.items()
-    descList.reverse()
-    return  descList
+    ascDic = sorted(dic.items(),key = operator.itemgetter(0),reverse = True)
+    return ascDic
 
 solution_asc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
 solution_desc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
